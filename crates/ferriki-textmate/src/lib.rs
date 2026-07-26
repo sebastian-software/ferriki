d2f6c5db4a153412b8aac18b3fc5ccf57f6c9098
Sebastian Werner <s.werner@sebastian-software.de>
2026-07-26 23:15:17 +0200

    feat(textmate): compile patterns with ferroni

--- crates/ferriki-textmate/src/lib.rs
+++ crates/ferriki-textmate/src/lib.rs
@@ -5,10 +5,15 @@
 
 pub mod matcher;
 pub mod raw_grammar;
+pub mod regexp;
 pub mod theme;
 
 pub use matcher::{create_matchers, Matcher, MatcherPriority, MatcherWithPriority};
 pub use raw_grammar::{Location, RawCaptures, RawGrammar, RawRepository, RawRule, RuleId};
+pub use regexp::{
+    has_captures, replace_captures, CaptureIndex, CompiledRule, FindNextMatchResult, OnigString,
+    RegExpSource, RegExpSourceList, ScannerFindOptions,
+};
 pub use theme::{
     font_style_to_string, parse_theme, FontStyle, ParsedThemeRule, RawTheme, RawThemeScope,
     RawThemeSetting, RawThemeStyle, ScopeStack, StyleAttributes, Theme, ThemeError,
